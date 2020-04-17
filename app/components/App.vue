ebf6754496a1609e896cf46146f3ac4b190dd871
harshithva <harshith11032001@gmail.com>
2020-04-17 20:23:19 +0530

    Initial commit

---
+++ app/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <Page>
+    <ActionBar title="My FirstApp!" />
+    <GridLayout columns="*" rows="*">
+      <Label class="message" :text="msg" col="0" row="0" />
+    </GridLayout>
+  </Page>
+</template>
+
+<script >
+export default {
+  data() {
+    return {
+      msg: "Hello Harshith!"
+    };
+  }
+};
+</script>
+
+<style scoped>
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
+
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
+</style>
